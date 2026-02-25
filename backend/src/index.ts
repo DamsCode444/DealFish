4312d56faffac1d8b28a3e15f12fa0b45eb2170f
Damodar Vaidik <baidikhero@gmail.com>
2026-02-25 16:20:00 +0800

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { ENV } from "./config/env";
+import cors from "cors";
+import { clerkMiddleware, clerkClient, requireAuth, getAuth } from '@clerk/express'
+
+
+
+const app = express()
+app.use(clerkMiddleware())// Adds Clerk authentication state and methods to the request object
+app.use(express.json()) //parse JSON bodies (as sent by API clients)    
+app.use(express.urlencoded({extended:true}))//parse urlencoded bodies (like HTML form submits)
+app.use(cors({origin:ENV.FRONTEND_URL}))// Enable CORS for all routes
+
+
+
+app.get("/",(req,res)=>
+    {
+        res.json({success:true,message:"Welcome to Productify API"})
+    })
+
+
+
+
+
+
+app.listen(ENV.PORT,()=> console.log("Server is running on port "+ENV.PORT))
